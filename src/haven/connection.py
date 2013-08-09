301fd60b4c865e60a05b0e8eaca9f8a89d0d6dde
Jon Bringhurst <jon@bringhurst.org>
2013-08-09 11:28:40 -0600

    Initial layout of a client REPL.

---
+++ src/haven/connection.py
@@ -0,0 +1,11 @@
+
+class HavenConnection():
+    def connect(self, server, port):
+        print("got to connect")
+
+    def disconnect(self):
+        print("got to disconnect")
+
+    def is_connected(self):
+        print("got to is_connected")
+        return True
---
+++ src/haven/haven.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import readline
+import cmd
+
+from connection import HavenConnection
+
+class HavenCtl(cmd.Cmd):
+    """Haven daemon controller REPL."""
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = "havenctl([disconnected])> "
+        self.intro  = "Haven daemon controller (0.0.1-alpha.1). Type help or ? to list commands."
+        self.conn = None
+
+    def do_disconnect(self, line):
+        """
+        disconnect
+            Disconnect from the current server.
+        """
+        if self.conn:
+            self.conn.disconnect()
+        else:
+            print("Not currently connected to a server. Please see 'help disconnect'.")
+        self.prompt = 'havenctl([disconnected])> '
+
+    def do_connect(self, server=None, port=7854):
+        """
+        connect <server> [port]
+            Connect to the specified server and port (default 7854).
+        """
+        if server:
+            if self.conn:
+                self.conn.disconnect()
+            self.prompt = 'havenctl([disconnected])> '
+
+            if not port:
+                port = 7854
+
+            print("Attempting to establish a connection to `" + server + ":" + str(port) + "'.")
+            self.conn = HavenConnection()
+            self.conn.connect(server, port)
+
+            if self.conn.is_connected():
+                print("Connection established.")
+                self.prompt = "havenctl(" + server + ":" + str(port) + ")> "
+            else:
+                print("Connection to server failed.")
+        else:
+            print("You must specify a server with the connect command. Please see 'help connect'.")
+
+    def do_EOF(self, line):
+        print("\nEncountered EOF. Exiting Haven daemon controller. Goodbye!")
+        return True
+
+    def do_exit(self, line):
+        print("Exiting Haven daemon controller. Goodbye!")
+        return True
+
+    def do_quit(self, line):
+        print("Quitting Haven daemon controller. Goodbye!")
+        return True
+
+    def postloop(self):
+        print
+
+if __name__ == '__main__':
+    HavenCtl().cmdloop()
