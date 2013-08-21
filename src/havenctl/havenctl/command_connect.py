64319640a0503ade54cf89964675870fc64aaf7f
Jon Bringhurst <jon@bringhurst.org>
2013-08-20 21:05:34 -0600

    Removed old protobuf code. Added more stubs for client commands. Turned down format string warnings.

---
+++ src/havenctl/havenctl/command_connect.py
@@ -0,0 +1,32 @@
+class CommandConnect():
+    """Haven connect command handler."""
+
+    def handle(self, ctl, line):
+        args = shlex.split(line)
+        server = None
+        port = 7854
+
+        if len(args) > 0:
+            server = args[0]
+        if len(args) > 1:
+            port = int(args[1])
+
+        if server:
+            if ctl.conn:
+                ctl.conn.disconnect()
+
+            ctl.prompt = PROMPT_DISCONNECTED
+            ctl.conn = HavenConnection()
+            ctl.conn.connect(server, port)
+
+            if ctl.conn.is_connected:
+                print("Connection established to `" + server + ":" + str(port) + "'.")
+                ctl.proto.send_connect(ctl.conn)
+                ctl.proto.recv_connect(ctl.conn)
+                ctl.prompt = "havenctl(" + server + ":" + str(port) + ")> "
+            else:
+                print("Connection to server failed.")
+        else:
+            print("You must specify a server with the connect command. Please see 'help connect'.")
+
+# EOF
