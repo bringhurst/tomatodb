f495ff1a8e19aaf206ef4b9a0e3487f224883e14
Jon Bringhurst <jon@bringhurst.org>
2013-08-30 00:06:40 -0600

    Added stub for func to persist all required state.

--- src/havend/replica.c
+++ src/havend/replica.c
@@ -19,6 +19,7 @@
 #include <dirent.h>
 
 #include "consensus.h"
+#include "pack/addr_struct.h"
 #include "pack/data_msg.h"
 #include "routing.h"
 #include "settings.h"
@@ -72,24 +73,12 @@ int HVN_replica_candidate(HVN_replica_t* replica, char* role)
 
 int HVN_replica_leader(HVN_replica_t* replica, char* role)
 {
-    char* op_packed;
-    size_t op_packed_len;
-    HVN_msg_client_data_t* client_data_msg;
-
-    HVN_INTENTIONALLY_UNUSED_VARIABLE(role);
+    UT_array* replicas;
+    UT_array* log;
+    HVN_addr_t* vote;
+    uint64_t term;
 
     LOG(HVN_LOG_INFO, "Replica has entered leader state.");
-
-    // Cache the local last log index from disk.
-    if(HVN_replica_cache_last_log_index(replica) != HVN_SUCCESS) {
-        LOG(HVN_LOG_ERR, "Could not determine last log index.");
-        return HVN_ERROR;
-    }
-    else {
-        LOG(HVN_LOG_INFO, "Last log index for this leader replica is `%zu'.", replica->last_log_index);
-    }
-
-    // TODO: Cache replica list from disk.
 
     // TODO: Skip until client command accept works -- so followers can join the quorum. Initialize nextIndex for each follower to the local last log index + 1.
 
@@ -98,14 +87,13 @@ int HVN_replica_leader(HVN_replica_t* replica, char* role)
 
     // Accept data commands from clients.
     LOG(HVN_LOG_DBG, "Leader replica is waiting for client data messages.");
-    client_data_msg = chanrecvp(replica->data_chan);
-
-    HVN_proto_print_data_msg(client_data_msg);
-    LOG(HVN_LOG_DBG, "Leader replica received a data msg.");
+    //client_data_msg = chanrecvp(replica->data_chan);
+    //HVN_proto_print_data_msg(client_data_msg);
+    //LOG(HVN_LOG_DBG, "Leader replica received a data msg.");
 
     // Pack and append to local log.
-    HVN_clnt_proto_pack_data_msgpack(client_data_msg, &op_packed_len, &op_packed);
-    HVN_replica_append_to_log(replica, op_packed, op_packed_len);
+    //HVN_clnt_proto_pack_data_msgpack(client_data_msg, &op_packed_len, &op_packed);
+    //HVN_replica_append_to_log(replica, op_packed, op_packed_len);
 
     // TODO: Whenever last log index is greater or equal to nextIndex for a follower:
     //           1. Send AppendEntries RPC with log entries starting at nextIndex.
@@ -122,62 +110,15 @@ int HVN_replica_leader(HVN_replica_t* replica, char* role)
     return HVN_SUCCESS;
 }
 
-int HVN_replica_cache_last_log_index(HVN_replica_t* replica)
+
+int HVN_replica_persist_state(HVN_replica_t* replica, \
+                              UT_array* log, \
+                              HVN_addr_t* term_vote, \
+                              uint64_t current_term)
 {
-    uint64_t* last_log_index;
+    //TODO persist log, vote, and term to leveldb.
 
-    if(HVN_db_unsafe_get_uint64(replica->db, \
-                                HVN_CONSENSUS_MD_LAST_LOG_INDEX, strlen(HVN_CONSENSUS_MD_LAST_LOG_INDEX),
-                                &last_log_index) != HVN_SUCCESS) {
-        LOG(HVN_LOG_ERR, "Could not determine the last log index.");
-        return HVN_ERROR;
-    }
-
-    replica->last_log_index = *last_log_index;
-    return HVN_SUCCESS;
-}
-
-int HVN_replica_overwrite_last_log_index(HVN_replica_t* replica, uint64_t last_log_index)
-{
-    if(HVN_db_unsafe_put_uint64(replica->db, \
-                                HVN_CONSENSUS_MD_LAST_LOG_INDEX, strlen(HVN_CONSENSUS_MD_LAST_LOG_INDEX), \
-                                last_log_index) != HVN_SUCCESS) {
-        LOG(HVN_LOG_ERR, "Could not overwrite the last log index for this replica.");
-        return HVN_ERROR;
-    }
-
-    return HVN_SUCCESS;
-}
-
-int HVN_replica_append_to_log(HVN_replica_t* replica, char* packed_op, size_t packed_op_len)
-{
-    char* err;
-    leveldb_writebatch_t* log_batch;
-
-    char* log_term_key = (char*) malloc(sizeof(char) * _POSIX_PATH_MAX);
-    char* log_cmd_key = (char*) malloc(sizeof(char) * _POSIX_PATH_MAX);
-
-    uint64_t* current_term = &(replica->current_term);
-
-    sprintf(log_term_key, HVN_CONSENSUS_MD_LOG_FMT_TERM, replica->current_term);
-    sprintf(log_cmd_key, HVN_CONSENSUS_MD_LOG_FMT_CMD, replica->last_log_index + 1);
-
-    log_batch = leveldb_writebatch_create();
-    leveldb_writebatch_put(log_batch, log_term_key, strlen(log_term_key), (char*) current_term, sizeof(uint64_t));
-    leveldb_writebatch_put(log_batch, log_cmd_key, strlen(log_cmd_key), packed_op, packed_op_len);
-    leveldb_write(replica->db->handle, replica->db->write_options, log_batch, &err);
-
-    if(HVN_replica_overwrite_last_log_index(replica, replica->last_log_index) != HVN_SUCCESS) {
-        LOG(HVN_LOG_ERR, "Could not update last log index.");
-        return HVN_ERROR;
-    }
-
-    replica->last_log_index++;
-
-    free(log_term_key);
-    free(log_cmd_key);
-
-    return HVN_SUCCESS;
+    return HVN_ERROR;
 }
 
 void HVN_replica_task(HVN_replica_t* replica)
