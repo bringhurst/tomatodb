871496493d59b2a1be08a448e1b288c14777f1d6
Jon Bringhurst <jon@bringhurst.org>
2013-09-06 10:15:31 -0600

    Implemented basic candidate flow.

--- src/havend/candidate.c
+++ src/havend/candidate.c
@@ -17,6 +17,8 @@
  */
 
 #include "log.h"
+#include "pack/append_msg.h"
+#include "pack/vote_msg.h"
 
 #include "candidate.h"
 
@@ -28,20 +30,69 @@ extern HVN_loglevel HVN_debug_level;
 
 int HVN_replica_candidate(HVN_replica_t* replica)
 {
-    LOG(HVN_LOG_INFO, "Replica has entered candidate state.");
+    HVN_msg_append_t append_msg;
+    HVN_msg_vote_t vote_msg;
 
-    HVN_INTENTIONALLY_UNUSED_VARIABLE(replica);
+    uint32_t timeout_result = 0;
+    unsigned int default_candidate_timeout = 5000;
+    static Alt alts[HVN_REPLICA_CANDIDATE_ALT_NK + 1];
+
+    LOG(HVN_LOG_INFO, "Replica has entered candidate state.");
 
     // TODO: Increment current Term, vote for self.
 
-    // TODO: Reset election timeout.
+    if(HVN_timer_init(&(replica->election_timer)) != HVN_SUCCESS) {
+        LOG(HVN_LOG_ERR, "Could not allocate an election timer for this candidate replica.");
+        return HVN_ERROR;
+    }
 
-    // TODO: Send RequestVote RPCs to all other servers, wait for either:
-    //           1. Votes received from majority of servers: become leader.
-    //           2. AppendEntries RPC received from new leader: step down.
-    //           3. Election timeout elapses without election resolution:
-    //               a. Increment term, state new election.
-    //           4. Discover higher term: step down.
+    HVN_timer_reset(replica->election_timer, default_candidate_timeout);
+
+    alts[HVN_REPLICA_CANDIDATE_ALT_APPEND_KEY].c = replica->append_chan_in;
+    alts[HVN_REPLICA_CANDIDATE_ALT_APPEND_KEY].v = &append_msg;
+    alts[HVN_REPLICA_CANDIDATE_ALT_APPEND_KEY].op = CHANRCV;
+
+    alts[HVN_REPLICA_CANDIDATE_ALT_TIMER_KEY].c = replica->election_timer->alarm_chan;
+    alts[HVN_REPLICA_CANDIDATE_ALT_TIMER_KEY].v = &timeout_result;
+    alts[HVN_REPLICA_CANDIDATE_ALT_TIMER_KEY].op = CHANRCV;
+
+    alts[HVN_REPLICA_CANDIDATE_ALT_VOTE_KEY].c = replica->vote_chan_in;
+    alts[HVN_REPLICA_CANDIDATE_ALT_VOTE_KEY].v = &vote_msg;
+    alts[HVN_REPLICA_CANDIDATE_ALT_VOTE_KEY].op = CHANRCV;
+
+    // TODO: Send RequestVote RPCs to all other servers
+
+    HVN_timer_start(replica->election_timer);
+
+    switch(chanalt(alts)) {
+
+        case HVN_REPLICA_CANDIDATE_ALT_APPEND_KEY:
+            LOG(HVN_LOG_DBG, "This candidate received an append message.");
+
+            // TODO: respond to requestor
+            // TODO: AppendEntries RPC received from new leader: step down.
+            break;
+
+        case HVN_REPLICA_CANDIDATE_ALT_TIMER_KEY:
+            LOG(HVN_LOG_DBG, "Received a candidate timeout alarm.");
+
+            // TODO: Increment term
+            // TODO: Start new election
+            break;
+
+        case HVN_REPLICA_CANDIDATE_ALT_VOTE_KEY:
+            LOG(HVN_LOG_DBG, "This candidate received a vote message.");
+
+            // TODO: respond to requestor
+            // TODO: If votes received from majority of servers: become leader.
+            break;
+
+        default:
+            LOG(HVN_LOG_ERR, "Unknown index received for candidate alt array.");
+            break;
+    }
+
+    // TODO: Discover higher term: step down (where does this go?).
 
     return HVN_ERROR;
 }
