44255cfe3439d40255f4b42a61dff9d3159d4075
Jon Bringhurst <jon@bringhurst.org>
2013-10-19 13:59:19 -0600

    Place and use a history file ino the home directory.

--- src/tomato/repl.c
+++ src/tomato/repl.c
@@ -1,13 +1,19 @@
+#include <pwd.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+#include "common.h"
 #include "repl.h"
 #include "task/task.h"
 
 void TDB_repl_cmd_task(char* cmd)
 {
     printf("task echo: '%s'\n", cmd);
+    // TODO: process cmd
 }
 
 void TDB_repl_completion(const char* buf, linenoiseCompletions* lc)
@@ -27,19 +33,53 @@ void TDB_repl_completion(const char* buf, linenoiseCompletions* lc)
     }
 }
 
+static int TDB_repl_hist_path(char** path)
+{
+    char* home_path = getenv("HOME");
+    struct passwd *pw;
+
+    *path = (char*) malloc(_POSIX_PATH_MAX * sizeof(char));
+
+    if(home_path == NULL) {
+        pw = getpwuid(getuid());
+        strncpy(*path, pw->pw_dir, _POSIX_PATH_MAX);
+        *path = strcat(*path, "/.tomato_history");
+
+        if(*path != NULL) {
+            return TDB_SUCCESS;
+        }
+    } else {
+        strncpy(*path, home_path, _POSIX_PATH_MAX);
+        *path = strcat(*path, "/.tomato_history");
+        return TDB_SUCCESS;
+    }
+
+    free(*path);
+    return TDB_ERROR;
+}
+
 void TDB_repl_start(void)
 {
     char* line;
+    char* hist_file;
+    bool history_enabled = false;
 
     linenoiseSetCompletionCallback(TDB_repl_completion);
-    linenoiseHistoryLoad("tomato_history"); /* Load the history at startup */
+
+    if(TDB_repl_hist_path(&hist_file) == TDB_SUCCESS) {
+        history_enabled = true;
+        linenoiseHistoryLoad(hist_file);
+    }
 
     while((line = linenoise("tomato> ")) != NULL) {
         if(line[0] != '\0' && line[0] != '/') {
             taskcreate((void (*)(void*)) TDB_repl_cmd_task, (void*) line, TDB_CMD_STACK_SIZE);
             taskyield();
-            linenoiseHistoryAdd(line);
-            linenoiseHistorySave("tomato_history");
+
+            if(history_enabled) {
+                linenoiseHistoryAdd(line);
+                linenoiseHistorySave(hist_file);
+            }
         }
         else if(!strncmp(line, "/historylen", 11)) {
             int len = atoi(line + 11);
@@ -52,3 +92,5 @@ void TDB_repl_start(void)
         free(line);
     }
 }
+
+/* EOF */
